38f8645634b98afe5c31a606130eba61f8e11471
M-Uzair25 <95268548+M-Uzair25@users.noreply.github.com>
2023-12-17 15:43:46 +0500

    Folders and file structure changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,51 @@
+'use client'
 import "@/styles/style.scss";
-
-export const metadata = {
-  title: 'Abdul Razzaq Plastic Dashboard',
-  description: 'Made by Muhammad Uzair',
-}
+import Head from "next/head";
+import React from "react";
+import { Container } from "reactstrap";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }) {
+  const [open, setOpen] = React.useState(false);
+  const showMobilemenu = () => {
+    setOpen(!open);
+  };
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Head>
+        <title>Abdul Razzaq Plastic Dashboard</title>
+        <meta name="description" content="Made by Muhammad Uzair" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <main>
+          <div className="pageWrapper d-md-block d-lg-flex">
+            {/******** Sidebar **********/}
+            <aside
+              className={`sidebarArea shadow bg-white ${!open ? "" : "showSidebar"
+                }`}
+            >
+              <Sidebar showMobilemenu={() => showMobilemenu()} />
+            </aside>
+            {/********Content Area**********/}
+
+            <div className="contentArea">
+              {/********header**********/}
+              <Header showMobmenu={() => showMobilemenu()} />
+
+              {/********Middle Content**********/}
+              <Container className="p-4 wrapper" fluid>
+                <div>{children}</div>
+              </Container>
+            </div>
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
