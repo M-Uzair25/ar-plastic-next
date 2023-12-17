6b574c7b3e084a0158053d7f3ae8e3e0313ab75f
M-Uzair25 <95268548+M-Uzair25@users.noreply.github.com>
2023-12-17 19:21:29 +0500

    mongodb connected successfully

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 const connection = {};
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/ARPlastic";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/AR_Plastic";
 
 export const connectToDB = async () => {
     try {
         if (connection.isConnected) return;
-        const db = await mongoose.connect(process.env.MONGO_URI);
+        const db = await mongoose.connect(MONGO_URI);
         connection.isConnected = db.connections[0].readyState;
         console.log("MongoDB connected successfully.");
     } catch (error) {
